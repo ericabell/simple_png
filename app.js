caa759e452a0399b09616acf584f5c023371ba64
crucalculus <crucalculus@gmail.com>
2016-08-01 21:01:53 -0500

    PNG image is now served!!!
    Excited about this:
    1. requests to the /db_image endpoint successfully get the png
    out of the database and return back that image.
    
    2. requests to /:questionId endpoint successfully get the png
    of the question in the database matching that ObjectId and
    spit back the image.
    
    A big learning for me was the fact that the image is stored
    as a Binary type in the database. I was trying to call img.length
    and set that value in the header, but in fact img.length is a function
    so I was getting a weird crashing message.
    
    Another big item was that my Python parsing script on my PC was
    calling f.open(filename, 'r') and so calls to .read() were broken
    by newlines. The fix was f.open(filename, 'rb')

--- app.js
+++ app.js
@@ -16,6 +16,17 @@ app.get('/', function (req, res) {
       res.send('Hello World!');
 });
 
+app.get('/number_of_records', function(req, res) {
+    MongoClient.connect(url, function(err,db) {
+        console.log("Connected successfully to server");
+        var collection = db.collection('question-collection');
+        collection.find({'compile_success': 'False'}).toArray(function(err, docs) {
+            res.send('Number of records with png image: ' + docs.length);
+            db.close();
+        });
+    });
+});
+
 app.get('/image', function( req, res) {
     var options = {
         root: __dirname,
@@ -37,29 +48,89 @@ app.get('/image', function( req, res) {
     });
 });
 
+app.get('/questions', function(req,res) {
+    MongoClient.connect(url, function(err,db) {
+        console.log("Connected successfully to server");
+        var collection = db.collection('question-collection');
+        collection.find().toArray(function(err, docs) {
+            res.send(docs);
+            db.close();
+        });
+    });    
+})
+
 app.get('/db_image', function(req, res) {
     MongoClient.connect(url, function(err,db) {
         console.log("Connected successfully to server");
         var collection = db.collection('question-collection');
-        var newObjectId = new ObjectID.createFromHexString("5782412affbf64df5fdcf1f6");
+        var newObjectId = new ObjectID.createFromHexString("5796aee0abdbe4138c2757d2");
         collection.find({'_id': newObjectId}).toArray(function(err, docs) {
             console.log(docs);
             // var newBuffer = Buffer.from(docs['png_image']);
             // var img = Binary(newBuffer);
             // var img = new Buffer(docs['png_image'], 'base64');
 
-            var img = Binary(docs[0]['png_image']);
+            // var img = Binary(docs[0]['png_image']);
+            var img = docs[0]['png_image']; // !!! img is of type Binary
+            console.log(img.length);
 
             res.writeHead(200, {
                 'Content-Type': 'image/png',
-                'Content-Length': img.length
+                'Content-Length': img.length() // if this was just .length, we will get a server error/crash
             });
-            res.end(img);
+            res.end(img.buffer, 'binary');
             // res.send(Binary(docs[0]['png_image']));
             db.close();
         });
     });
 });
+
+app.get('/:questionId', function(req, res) {
+  MongoClient.connect(url, function(err,db){
+    console.log("Connected successfully to the database");
+    var collection = db.collection('question-collection');
+      // find the one document corresponding to the Object ID passed by GET
+      var newObjectId = new ObjectID.createFromHexString(req.params.questionId);
+      collection.find({'_id': newObjectId} ).toArray(function(err, docs) {
+            var img = docs[0]['png_image']; // !!! img is of type Binary
+            console.log(img.length);
+
+            res.writeHead(200, {
+                'Content-Type': 'image/png',
+                'Content-Length': img.length() // if this was just .length, we will get a server error/crash
+            });
+            res.end(img.buffer, 'binary');
+            // res.send(Binary(docs[0]['png_image']));
+            db.close();
+        
+        db.close();
+      });
+  });
+});
+
+app.get('/test', function(req, res) {
+    MongoClient.connect(url, function(err,db) {
+        console.log("Connected successfully to server");
+        var collection = db.collection('question-collection');
+        var newObjectId = new ObjectID.createFromHexString("5796aee0abdbe4138c2757d2");
+        collection.find({'_id': newObjectId}).toArray(function(err, docs) {
+            // console.log(docs);
+
+            // var img = docs[0]['png_image']['buffer'];
+            // res.writeHead(200, {
+                // 'Content-Type': 'image/png',
+                // 'Content-Length': img.length
+            // });
+            // res.end(Buffer(img));
+            // console.log(img);
+
+            res.send(docs);
+
+            db.close();
+        });
+    });
+});
+
 
 app.listen(3000, function () {
       console.log('Example app listening on port 3000!');
